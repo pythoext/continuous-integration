80beab21c26d3359fadd25a328337e60778c21bd
Matteo Balasso <matteo.balasso@prometeia.com>
2014-11-05 12:16:51 +0100

    first commit

---
+++ travis/install.sh
@@ -0,0 +1,11 @@
+MINICONDA_URL="http://repo.continuum.io/miniconda"
+MINICONDA_FILE="Miniconda-3.5.5-Linux-x86_64.sh"
+wget "${MINICONDA_URL}/${MINICONDA_FILE}"
+bash $MINICONDA_FILE -b
+
+export PATH=$HOME/miniconda/bin:$PATH
+
+conda install --yes pip conda-build jinja2 binstar
+conda update --yes conda
+conda config --add channels http://conda.binstar.org/prometeia
+conda config --add channels http://conda.binstar.org/t/$BINSTAR_TOKEN/prometeia
