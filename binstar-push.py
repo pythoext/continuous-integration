30ff5f15f17b93ea44d935eb3c16e0ec75e2fdba
Matteo Balasso <matteo.balasso@prometeia.com>
2014-11-10 17:22:42 +0100

    update in binstar-push script #2

--- binstar-push.py
+++ binstar-push.py
@@ -5,9 +5,8 @@ import traceback
 
 token = os.environ['BINSTAR_TOKEN']
 channel = os.environ.get('BINSTAR_CHANNEL', 'main')
-cmd = ['binstar', '-t', token, 'upload', '--force']
+cmd = ['binstar', '-t', token, 'upload', '-c %s' % channel, '--force']
 cmd.extend(glob.glob('*.tar.bz2'))
-cmd.extend(['--channel %s' % channel])
 try:
     subprocess.check_call(cmd)
 except subprocess.CalledProcessError:
