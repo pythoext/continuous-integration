bbf55398ab4d43111b0f8fd8154f4b76d289da58
Matteo Balasso <matteo.balasso@prometeia.com>
2014-11-13 12:15:06 +0100

    add trigger_builds script

---
+++ trigger_builds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir $1
+cd $1
+git init
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+git remote add origin https://github.com/prometeia/$1.git
+git pull
+git checkout master
+touch build_trigger_number
+touch build_trigger_branch
+echo $TRAVIS_BRANCH > build_trigger_branch
+echo $TRAVIS_BUILD_NUMBER > build_trigger_number
+git add --all .
+git commit -m "Automated push to trigger a new build"
+git push origin master
+echo "OK!"
+cd ..
