03f21b56aaf4ffe17b6ec837e33143c2a39766bf
Matteo Balasso <matteo.balasso@prometeia.com>
2014-11-14 11:10:58 +0100

    add channels mng

--- update_build_settings.py
+++ update_build_settings.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+branch_to_channel = {
+    'develop': 'dev',
+    'ratingpro': 'ratingpro2',
+    'tg_dj_oo': 'new-ui'
+}
+
 try:
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
     BRANCH_TO_BUILD = open(os.path.join(BASE_DIR, 'build_trigger_branch'), 'r').read().strip()
@@ -9,6 +15,12 @@ except IOError:
     BRANCH_TO_BUILD = "master"
 
 if 'win' in sys.platform:
-    print "SET BRANCH_TO_BUILD=%s" % BRANCH_TO_BUILD
+    cmd = "SET"
 else:
-    print "export BRANCH_TO_BUILD=\"%s\"" % BRANCH_TO_BUILD
+    cmd = "export"
+
+out = "%s BRANCH_TO_BUILD=%s" % (cmd, BRANCH_TO_BUILD)
+if BRANCH_TO_BUILD in branch_to_channel:
+    out += " && %s SET BINSTAR_CHANNEL=%s" % (cmd, branch_to_channel[BRANCH_TO_BUILD])
+
+print out
