712f54da85d41b5105a2fa88d4ad385bd170b322
Matteo Balasso <matteo.balasso@prometeia.com>
2015-07-14 17:27:04 +0200

    - add BUILD_NUMBER to exported build settings (to align version number of pytho package to others)

--- update_build_settings.py
+++ update_build_settings.py
@@ -3,18 +3,24 @@ import yaml
 import sys
 from config import BRANCH_TO_CHANNEL
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 try:
-    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
     BRANCH_TO_BUILD = open(os.path.join(BASE_DIR, 'build_trigger_branch'), 'r').read().strip()
 except IOError:
     BRANCH_TO_BUILD = "master"
+
+try:
+    BUILD_NUMBER = open(os.path.join(BASE_DIR, 'build_trigger_number'), 'r').read().strip()
+except IOError:
+    BUILD_NUMBER = os.environ.get('TRAVIS_BUILD_NUMBER')
 
 if 'win' in sys.platform:
     cmd = "SET"
 else:
     cmd = "export"
 
-output = "%s BRANCH_TO_BUILD=%s" % (cmd, BRANCH_TO_BUILD)
+output = "%s BRANCH_TO_BUILD=%s && %s BUILD_NUMBER=%s" % (cmd, BRANCH_TO_BUILD, cmd, BUILD_NUMBER)
 
 if 'master' not in BRANCH_TO_BUILD:
     # add channel if needed
