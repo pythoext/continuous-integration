d48e4ca52af600d8b376dd5644cb97cd497bc793
Matteo Balasso <matteo.balasso@prometeia.com>
2015-08-26 10:04:31 +0200

    - bug with conda-build 1.17 (https://github.com/conda/conda-build/issues/555) set version to 1.16 in install scripts

--- travis/install.sh
+++ travis/install.sh
@@ -5,7 +5,7 @@ bash $MINICONDA_FILE -b
 
 export PATH=$HOME/miniconda/bin:$PATH
 
-conda install --yes pip conda-build jinja2 anaconda-client
+conda install --yes pip jinja2 anaconda-client conda-build==1.16
 conda update --yes conda
 conda config --add channels https://conda.anaconda.org/prometeia
 conda config --add channels https://conda.anaconda.org/t/$BINSTAR_TOKEN/prometeia
