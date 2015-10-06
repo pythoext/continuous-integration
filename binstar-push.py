7cc31b48fe42ca63798741c8ca7d8e6ce59ef6d3
Matteo Balasso <matteo.balasso@prometeia.com>
2015-10-06 11:13:30 +0200

    - update binstar_push to receive channel via sys.argv

--- binstar-push.py
+++ binstar-push.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import glob
 import subprocess
 import traceback
@@ -6,18 +7,25 @@ from config import BRANCH_TO_CHANNEL
 token = os.environ['BINSTAR_TOKEN']
 cmd = ['binstar', '-t', token, 'upload']
 
-# SET CHANNEL
-try:
-    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-    BRANCH_TO_BUILD = open(os.path.join(BASE_DIR, 'build_trigger_branch'), 'r').read().strip()
-except IOError:
-    BRANCH_TO_BUILD = "master"
+target_channel = None
 
-if "master" not in BRANCH_TO_BUILD:
-    cmd.extend(['--channel=%s' % BRANCH_TO_CHANNEL.get(BRANCH_TO_BUILD, BRANCH_TO_BUILD)])
+if len(sys.argv) > 1:
+    target_channel = sys.argv[1]
+else:
+    # SET CHANNEL
+    try:
+        BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+        BRANCH_TO_BUILD = open(os.path.join(BASE_DIR, 'build_trigger_branch'), 'r').read().strip()
+        target_channel = BRANCH_TO_CHANNEL.get(BRANCH_TO_BUILD, BRANCH_TO_BUILD)
+    except IOError:
+        target_channel = None
+
+if target_channel is not None:
+    cmd.extend(['--channel=%s' % target_channel ])
 
 cmd.extend(['--force'])
 cmd.extend(glob.glob('*.tar.bz2'))
+
 try:
     subprocess.check_call(cmd)
 except subprocess.CalledProcessError:
