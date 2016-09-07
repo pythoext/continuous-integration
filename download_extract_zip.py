dcc39005c879b3e414f5978f92de48d79aea2387
Matteo Balasso <matteo.balasso@prometeia.com>
2016-09-07 14:09:35 +0200

    - add donwload and extract zip script

---
+++ download_extract_zip.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import zipfile
+import requests
+
+# usage download_extract_zip path/to/extract/folder
+
+BASE_DIR = os.path.dirname(__file__)
+
+dest_dir = sys.argv[1]
+
+if not os.path.isabs(dest_dir):
+    dest_dir = os.path.join(BASE_DIR, dest_dir)
+
+if not os.path.exists(dest_dir):
+    raise RuntimeError("Destination path path not found: %s" % dest_dir)
+
+url = open(os.path.join(BASE_DIR, 'url')).read().strip()
+src_file_path = os.path.join(BASE_DIR, 'src.zip')
+
+f = open(src_file_path, "wb")
+r = requests.get(url)
+f.write(r.content)
+f.close()
+
+z = zipfile.ZipFile(src_file_path, "r")
+z.extractall(dest_dir)
