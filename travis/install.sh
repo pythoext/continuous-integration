4cf206e1ff59954f27e9546ebfbb6a2df86f5212
Matteo Balasso <balasoft.inc@gmail.com>
2016-10-24 16:52:01 +0200

    downgrade anaconda-client to 1.2.1

--- travis/install.sh
+++ travis/install.sh
@@ -6,6 +6,6 @@ bash $MINICONDA_FILE -b
 export PATH=$HOME/miniconda/bin:$HOME/miniconda2/bin:$PATH
 #dalla version conda >=4.2 non esiste piu' la get_proxy_server dal modulo config 
 #facendo rompere tutte le nostre  build!!! 
-conda install --yes pip jinja2 anaconda-client conda-build==1.16 conda==4.1.12
+conda install --yes pip jinja2 anaconda-client==1.2.1 conda-build==1.16 conda==4.1.12
 conda config --add channels https://conda.anaconda.org/prometeia
 conda config --add channels https://conda.anaconda.org/t/$BINSTAR_TOKEN/prometeia
