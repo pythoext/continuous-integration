2b85aa28ebb3e60ae8f5c1b5b9d9432f8a6b1419
Alfredo Saglimbeni <alfredo.saglimbeni@gmail.com>
2016-11-04 17:48:01 +0100

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -7,7 +7,7 @@ export PATH=$HOME/miniconda/bin:$HOME/miniconda2/bin:$PATH
 #dalla version conda >=4.2 non esiste piu' la get_proxy_server dal modulo config 
 #facendo rompere tutte le nostre  build!!! 
 conda install --yes pip==8.1.2 jinja2 anaconda-client==1.2.1 conda-build==1.16 conda==4.1.12
-conda update -n root conda-build
+conda update --yes -n root conda-build
 conda install --yes pip==8.1.2
 conda config --add channels https://conda.anaconda.org/prometeia
 conda config --add channels https://conda.anaconda.org/t/$BINSTAR_TOKEN/prometeia
