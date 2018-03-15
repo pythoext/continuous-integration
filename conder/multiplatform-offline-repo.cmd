c2acfb479502c178287a1fee47cc4a5e1cfd912c
Denis Brandolini <denis.brandolini@prometeia.com>
2018-03-15 15:23:25 +0100

    #1 lo script multiplatform-offline-repo builda tutto partendo dalle label pytho/pytho_dev.

--- conder/multiplatform-offline-repo.cmd
+++ conder/multiplatform-offline-repo.cmd
@@ -1,15 +1,26 @@
 @ECHO OFF
-IF [%4] == [] GOTO usage
-SET distro=%1
-SET distrotoken=%2
-SET mainrepo=%3
+IF [%2] == [] GOTO usage
+SET distrotoken=%1
+SET mainrepo=%2
+SET applications=pytho ratingpro
 
+ECHO Installing prerequisites...
 docker build . -t linuxbuilder
-docker run -e DONTINDEX=DONTINDEX -e REPO=%mainrepo% -e DISTRO=%distro% -e TOKEN=%distrotoken% -e APPLICATIONS="%4 %5 %6 %7 %8 %9" -v %mainrepo%:/pythomainrepo --name conderbuilder --rm linuxbuilder
+call conda install -y anaconda-client
 
-call update-offline-repo %1 %2 %3 %4 %5 %6 %7 %8 %9
+ECHO Extracting labels to package...
+call anaconda -t %distrotoken% label --list 2> labelsout.tmp
+findstr /R /C:"pytho_dev_[0-9][0-9]_[0-9][0-9]" labelsout.tmp | sort > labels.tmp
+findstr /R /C:"pytho_[0-9][0-9]_[0-9][0-9]" labelsout.tmp | sort >> labels.tmp
 
+FOR /F "tokens=2 delims= " %%G IN (labels.tmp) DO (
+    echo ====== Offline packaging %applications% label %%G ========
+    docker run -e DONTINDEX=DONTINDEX -e REPO=%mainrepo% -e DISTRO=%%G -e TOKEN=%distrotoken% -e APPLICATIONS="%applications%" -v %mainrepo%:/pythomainrepo --name conderbuilder --rm linuxbuilder
+    call update-offline-repo %%G %distrotoken% %mainrepo% %applications%
+)
+
+echo ===== DONE ====== 
 exit /b 0
 
 :usage
-echo "USAGE: mutiplatfom-offline-repo <label> <token accesso> <root del repository offline> <pacchetto1> <...> <pacchettoN>"
+echo "USAGE: mutiplatfom-offline-repo <anaconda token> <offline repository root>"
