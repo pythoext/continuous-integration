f8df0c6aba71fcbd5332ad59283a36a3c50dec2b
Denis Brandolini <denis.brandolini@prometeia.com>
2018-07-05 16:03:29 +0200

    Risoluzione senza meta.yaml del pacchetto

--- move-conda-package.py
+++ move-conda-package.py
@@ -9,10 +9,7 @@ from conda_build.config import Config
 #    from conda_build import config
 config = Config()
 
-with open(os.path.join(sys.argv[1], 'meta.yaml')) as f:
-    name = yaml.load(f)['package']['name']
-
-binary_package_glob = os.path.join(config.bldpkgs_dir, '{0}*.tar.bz2'.format(name))
-binary_package = glob.glob(binary_package_glob)[0]
+binary_package_glob = os.path.join(config.bldpkgs_dir, '*.tar.bz2')
+binary_package = sorted(glob.glob(binary_package_glob))[-1]
 
 shutil.move(binary_package, '.')
